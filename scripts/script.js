61325150d54d208945ed9fe713cc48e1d937b382
Reid Crawford <reidcrawford55@gmail.com>
2018-04-11 01:24:00 -0700

    finished implementing jQuery isotope

--- scripts/script.js
+++ scripts/script.js
@@ -22,3 +22,21 @@ $('a[data-filter=".design"]').on('click', function(event){
 	  filter: '.design'
 	});
 })
+
+$('a[data-filter=".coding"]').on('click', function(event){
+	$('.grid').isotope({
+	  // options
+	  itemSelector: '.grid-item',
+	  layoutMode: 'fitRows',
+	  filter: '.coding'
+	});
+})
+
+$('a[data-filter=".logo"]').on('click', function(event){
+	$('.grid').isotope({
+	  // options
+	  itemSelector: '.grid-item',
+	  layoutMode: 'fitRows',
+	  filter: '.logo'
+	});
+})
