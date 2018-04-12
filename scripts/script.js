f43f19a36e93d6ef5a7336e5078d8ab10e1c4461
Reid Crawford <reidcrawford55@gmail.com>
2018-04-11 19:34:59 -0700

    added items and style switcher

--- scripts/script.js
+++ scripts/script.js
@@ -1,58 +1,70 @@
-$('.grid').isotope({
-  // options
-  itemSelector: '.grid-item',
-  layoutMode: 'fitRows'
+$(document).ready(function(){
+	$('.grid').isotope({
+	  // options
+	  itemSelector: '.grid-item',
+	  layoutMode: 'fitRows'
+	});
+
+
+	$('a[data-filter="*"]').on('click', function(event){
+		$('.grid').isotope({
+		  // options
+		  itemSelector: '.grid-item',
+		  layoutMode: 'fitRows',
+		  filter: '*'
+		});
+	})
+
+	$('a[data-filter=".design"]').on('click', function(event){
+		$('.grid').isotope({
+		  // options
+		  itemSelector: '.grid-item',
+		  layoutMode: 'fitRows',
+		  filter: '.design'
+		});
+	})
+
+	$('a[data-filter=".coding"]').on('click', function(event){
+		$('.grid').isotope({
+		  // options
+		  itemSelector: '.grid-item',
+		  layoutMode: 'fitRows',
+		  filter: '.coding'
+		});
+	})
+
+	$('a[data-filter=".logo"]').on('click', function(event){
+		$('.grid').isotope({
+		  // options
+		  itemSelector: '.grid-item',
+		  layoutMode: 'fitRows',
+		  filter: '.logo'
+		});
+	})
+
+	$('a[href^="#"]').on('click', function(e) {
+		e.preventDefault();
+	});
+
+	var navItems = $("#filters").find('a');
+
+
+	  for ( var i = 0; i < navItems.length; i++ ) (function(i){ 
+	      navItems[i].onclick = function() {
+	      console.log(navItems[i]);
+	      console.log(this);
+	      $("#filters a").removeClass('current');
+	      $(this).fadeIn().addClass('current');
+	   }
+	  })(i);
+
+	  // look style swither
+
+
+	  var styleSwitches = $(".skin-part ul").find('a');
+
+	  for (var i = 0; i < styleSwitches.length; i++) {
+	  		console.log(styleSwitches[i]);
+	  }
+
 });
-
-
-$('a[data-filter="*"]').on('click', function(event){
-	$('.grid').isotope({
-	  // options
-	  itemSelector: '.grid-item',
-	  layoutMode: 'fitRows',
-	  filter: '*'
-	});
-})
-
-$('a[data-filter=".design"]').on('click', function(event){
-	$('.grid').isotope({
-	  // options
-	  itemSelector: '.grid-item',
-	  layoutMode: 'fitRows',
-	  filter: '.design'
-	});
-})
-
-$('a[data-filter=".coding"]').on('click', function(event){
-	$('.grid').isotope({
-	  // options
-	  itemSelector: '.grid-item',
-	  layoutMode: 'fitRows',
-	  filter: '.coding'
-	});
-})
-
-$('a[data-filter=".logo"]').on('click', function(event){
-	$('.grid').isotope({
-	  // options
-	  itemSelector: '.grid-item',
-	  layoutMode: 'fitRows',
-	  filter: '.logo'
-	});
-})
-
-$('a[href^="#"]').on('click', function(e) {
-	e.preventDefault();
-});
-
-var navItems = $("#filters").find('a');
-
-
-  for ( var i = 0; i < navItems.length; i++ ) (function(i){ 
-      navItems[i].onclick = function() {
-      console.log(navItems[i]);
-      console.log(this);
-      $("#filters a").removeClass('current');
-      $(this).fadeIn().addClass('current');
-   }
-  })(i);
